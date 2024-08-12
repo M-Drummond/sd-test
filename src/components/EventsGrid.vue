e0d8df0c1b2bb2179e47f202709fd4a59ec24f62
Mackinley <mackinley@punchbuggy.com.au>
2024-08-12 18:51:32 +1000

    layout tweaks

--- src/components/EventsGrid.vue
+++ src/components/EventsGrid.vue
@@ -8,7 +8,7 @@ const events = Events.events as EventData[]
 </script>
 <template>
   <main
-    class="grid site-width grid-cols-1 sm:gap-x-4 gap-y-[25px] md:gap-y-[31px] lg:gap-x-[45px] sm:grid-cols-1 md:grid-cols-3 py-8"
+    class="grid site-width grid-cols-1 sm:gap-x-4 gap-y-[25px] md:gap-y-[31px] lg:gap-x-[45px] sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-8"
   >
     <EventCard v-for="(event, index) in events" :key="`${index}__${event.title}`" :event />
   </main>
